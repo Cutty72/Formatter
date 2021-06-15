34e4d59d6d4e1795d2415ebdecbe51b979e53b21
Cutty72 <84432768+Cutty72@users.noreply.github.com>
2021-06-14 22:44:50 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+  
 const readline = require('readline');
 const chalk = require('chalk');
 const { convHeader } = require('./converter')
@@ -8,19 +9,24 @@ const rline = readline.createInterface({
     output: process.stdout
 });
 
+
+const headers = [];
+
 async function convertHeaders() {
     rline.question('Paste Headers here: ', (header) => {
+        headers.push(header);
+        if (header.length ){
         if (header === '') { rline.close(); return; }
 
         try {
             console.log(chalk.green(convHeader(header)))
+
         }
         catch { console.log(chalk.red.underline("Failed to convert Headers!")); }
-
         rline.close();
+    }
     })
 }
 
 module.exports = { convertHeaders }
-
 
